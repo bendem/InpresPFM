433675b132c567dd4918b0d67f6680b1f8169209
bendem <online@bendem.be>
2015-09-23 21:21:04 +0200

    Build script of the lazy people

---
+++ build.sh
@@ -0,0 +1,28 @@
+function run_gradle() {
+    local path=${1:0:(-1)}
+    cd $path
+    echo Running gradle in $(pwd)
+    gradle build &> ../$path.log
+}
+
+function run_cmake() {
+    local path=${1:0:(-1)}
+    cd $path
+    if [ ! -d "build" ]; then
+        mkdir build
+    fi
+    echo Running cmake in $(pwd)
+    cd build
+    cmake .. &> ../../$path.log
+    make &>> ../../$path.log
+}
+
+for f in */; do
+    if [ -f "$f/build.gradle" ]; then
+        run_gradle $f&
+    else
+        if [ -f "$f/CMakeLists.txt" ]; then
+            run_cmake $f&
+        fi
+    fi
+done
