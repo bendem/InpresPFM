0e9c21b09455f897832f873dfd33431eb1b45cbe
Curlybear <erwanflohic@gmail.com>
2015-10-07 18:39:00 +0000

    Add user creation script

---
+++ bd-traffic/creation_user.sql
@@ -0,0 +1,26 @@
+create role role_traffic not identified;
+grant alter session to role_traffic;
+grant create database link to role_traffic;
+grant create session to role_traffic;
+grant create procedure to role_traffic;
+grant create sequence to role_traffic;
+grant create table to role_traffic;
+grant create trigger to role_traffic;
+grant create type to role_traffic;
+grant create synonym to role_traffic;
+grant create view to role_traffic;
+grant create job to role_traffic;
+grant create materialized view to role_traffic;
+
+create user dbtraffic identified by &1
+    default tablespace users
+    temporary tablespace temp
+    profile default
+    account unlock;
+alter user dbtraffic quota unlimited on users;
+grant role_traffic to dbtraffic;
+
+grant execute on dbms_lock to dbtraffic;
+grant execute on sys.owa_opt_lock to dbtraffic;
+
+exit
