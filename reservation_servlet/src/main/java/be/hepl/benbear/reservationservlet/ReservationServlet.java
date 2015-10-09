5e7be998ce00be673689ea5aadf998849136bd0f
bendem <online@bendem.be>
2015-10-09 22:04:07 +0200

    Add servlet dependencies

--- reservation_servlet/src/main/java/be/hepl/benbear/reservationservlet/ReservationServlet.java
+++ reservation_servlet/src/main/java/be/hepl/benbear/reservationservlet/ReservationServlet.java
@@ -1,5 +1,7 @@
 package be.hepl.benbear.reservationservlet;
 
+import be.hepl.benbear.commons.db.Database;
+
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -8,6 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class ReservationServlet extends HttpServlet {
+
+    public ReservationServlet() {
+        new Database();
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
