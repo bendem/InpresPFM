5104f3b58c32de12411d9dafd855a10616686a7b
bendem <online@bendem.be>
2015-10-11 22:19:24 +0200

    Simplify servlet code and fix html

--- reservation_servlet/src/main/java/be/hepl/benbear/reservationservlet/LoginServlet.java
+++ reservation_servlet/src/main/java/be/hepl/benbear/reservationservlet/LoginServlet.java
@@ -3,19 +3,21 @@ package be.hepl.benbear.reservationservlet;
 import be.hepl.benbear.commons.db.DBPredicate;
 import be.hepl.benbear.commons.db.Database;
 import be.hepl.benbear.commons.db.Table;
-import be.hepl.benbear.trafficdb.*;
+import be.hepl.benbear.trafficdb.Destination;
+import be.hepl.benbear.trafficdb.User;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class LoginServlet extends HttpServlet {
 
@@ -44,64 +46,59 @@ public class LoginServlet extends HttpServlet {
     }
 
     protected void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, InterruptedException, ExecutionException, TimeoutException {
-        Table<User> table = database.table(User.class);
-        HttpSession session = req.getSession();
-        Object logged = session.getAttribute("logged");
-        String username = req.getParameter("username");
-        String password = req.getParameter("password");
-        if (logged != null) {
+        if (req.getSession().getAttribute("logged") != null) {
             proceedToReservation(req, resp);
             return;
         }
 
-        if (username != null && password != null) {
-            if ("on".equals(req.getParameter("newuser"))) {
-                table.insert(new User(0, username, password)).get();
-                proceedToReservation(req, resp);
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        if(username == null || password == null) {
+            resp.sendRedirect("login.html");
+            return;
+        }
+
+        Table<User> table = database.table(User.class);
+        if ("on".equals(req.getParameter("newuser"))) {
+            table.insert(new User(0, username, password)).get();
+        } else {
+            Optional<User> user = table.findOne(DBPredicate.of("username", username)).get(5, TimeUnit.SECONDS);
+            if(!user.isPresent() || !user.get().getPassword().equals(password)) {
+                resp.sendRedirect("login.html");
                 return;
-            } else {
-                Optional<User> quser = table.find(DBPredicate.of("username", username)).get(5, TimeUnit.SECONDS).findFirst();
-                if (quser.isPresent()) {
-                    User user = quser.get();
-                    if (user.getPassword().equals(password)) {
-                        proceedToReservation(req, resp);
-                        return;
-                    }
-                }
             }
         }
-        resp.sendRedirect("login.html");
+
+        proceedToReservation(req, resp);
     }
 
     protected void proceedToReservation(HttpServletRequest req, HttpServletResponse resp) throws IOException, ExecutionException, InterruptedException {
         HttpSession sess = req.getSession();
+        Object noSpace = sess.getAttribute("noSpace");
+        PrintWriter out = resp.getWriter();
+        Table<Destination> table = database.table(Destination.class);
 
+        resp.setContentType("text/html;charset=UTF-8");
         sess.setAttribute("logged", true);
 
-        Object noSpace = sess.getAttribute("noSpace");
-        resp.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = resp.getWriter();
-        out.println("<html><head><title>");
-        out.println("Reservation");
-        out.println("</title></head><body>");
+        out.println("<html><head><title>Reservation</title></head><body>");
 
         if (noSpace == null) {
-
             out.println("<p>Reservation form</p>");
             out.println("<form method=\"post\" action=\"ServletRes\">");
             out.println("<p>Arrival date : <input type=\"date\" name=\"dateArrival\"></p>");
-            out.println("<p>Destination : </p><select name=\"destination\">");
 
-            Table<Destination> table = database.table(Destination.class);
-            table.find().get().forEach(row -> out.println("<option value=\"" + row.getDestinationId() + "\">" + row.getCity() + "</option>"));
+            out.println("<p>Destination : <select name=\"destination\">");
+            table.find().get().forEach(dest -> out.printf("<option value=\"%d\">%s</option>", dest.getDestinationId(), dest.getCity()));
+            out.println("</select></p>");
 
-            out.println("</select>");
-            out.println("<input type=\"submit\" value=\"Submit\">");
-            out.println("</body></html>");
+            out.println("<input type=\"submit\">");
+            out.println("</form>");
         } else {
             out.println("<p>No more space available</p>");
-            out.println("</body></html>");
         }
+        out.println("</body></html>");
     }
 
     @Override
