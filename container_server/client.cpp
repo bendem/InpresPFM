defda19126bfbd3e3678140d30b06134604d7c40
bendem <online@bendem.be>
2015-10-12 08:30:21 +0200

    Don't hardcode target host

--- container_server/client.cpp
+++ container_server/client.cpp
@@ -2,12 +2,16 @@
 
 int main(int argc, char** argv) {
     unsigned short port = 31060;
+    std::string host("localhost");
     if(argc >= 2) {
         port = atoi(argv[1]);
     }
+    if(argc >= 3) {
+        host = argv[2];
+    }
 
     std::shared_ptr<Socket> s(new Socket);
-    s->connect(port, "localhost");
+    s->connect(port, host);
 
     CMMPTranslator translator;
     ProtocolHandler<CMMPTranslator, PacketId> proto(translator);
