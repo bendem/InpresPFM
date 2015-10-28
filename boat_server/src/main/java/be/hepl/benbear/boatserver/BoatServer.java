cbd77b997393f185c1d5f140c99205f56a7a0992
bendem <online@bendem.be>
2015-10-28 19:50:27 +0100

    Add login check in boat_server

--- boat_server/src/main/java/be/hepl/benbear/boatserver/BoatServer.java
+++ boat_server/src/main/java/be/hepl/benbear/boatserver/BoatServer.java
@@ -1,5 +1,8 @@
 package be.hepl.benbear.boatserver;
 
+import be.hepl.benbear.accounting_db.Staff;
+import be.hepl.benbear.commons.db.DBPredicate;
+import be.hepl.benbear.commons.db.Database;
 import be.hepl.benbear.commons.net.Server;
 import be.hepl.benbear.commons.streams.UncheckedLambda;
 import be.hepl.benbear.iobrep.LoginPacket;
@@ -10,13 +13,18 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class BoatServer extends Server<ObjectInputStream, ObjectOutputStream> {
 
+    private final Database accounting;
     private final Set<UUID> sessions;
 
     public BoatServer(int port, ExecutorService threadPool) {
@@ -27,6 +35,16 @@ public class BoatServer extends Server<ObjectInputStream, ObjectOutputStream> {
             UncheckedLambda.function(ObjectInputStream::new, Throwable::printStackTrace),
             UncheckedLambda.function(ObjectOutputStream::new, Throwable::printStackTrace)
         );
+
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch(ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        this.accounting = new Database();
+        this.accounting.registerClass(Staff.class);
+        this.accounting.connect("jdbc:oracle:thin:@178.32.41.4:8080:xe", "accounting", "bleh");
         this.sessions = new CopyOnWriteArraySet<>();
     }
 
@@ -63,6 +81,25 @@ public class BoatServer extends Server<ObjectInputStream, ObjectOutputStream> {
 
     private void handleLogin(LoginPacket packet, ObjectOutputStream os) throws IOException {
         System.out.printf("%s:%s%n", packet.getUsername(), packet.getPassword());
+        Optional<Staff> user;
+        try {
+            user = accounting
+                .table(Staff.class)
+                .findOne(DBPredicate.of("login", packet.getUsername()))
+                .get(5, TimeUnit.SECONDS);
+        } catch(InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return;
+        } catch(ExecutionException | TimeoutException e) {
+            os.writeObject(new LoginResponsePacket(null, "An error happened: " + e.getMessage()));
+            return;
+        }
+
+        if(!user.isPresent() || !user.get().getPassword().equals(packet.getPassword())) {
+            os.writeObject(new LoginResponsePacket(null, "Unknown login or password"));
+            return;
+        }
+
         UUID session = UUID.randomUUID();
         sessions.add(session);
         os.writeObject(new LoginResponsePacket(session, null));
