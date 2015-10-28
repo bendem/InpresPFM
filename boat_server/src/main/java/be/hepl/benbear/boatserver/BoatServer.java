5d49cb78ff3b11e1a6f972f1ace35951f54eb95a
bendem <online@bendem.be>
2015-10-28 18:06:38 +0100

    Rewrite BoatServer

--- boat_server/src/main/java/be/hepl/benbear/boatserver/BoatServer.java
+++ boat_server/src/main/java/be/hepl/benbear/boatserver/BoatServer.java
@@ -1,10 +1,12 @@
 package be.hepl.benbear.boatserver;
 
 import be.hepl.benbear.commons.net.Server;
-import be.hepl.benbear.iobrep.AuthenticatedPacket;
+import be.hepl.benbear.commons.streams.UncheckedLambda;
+import be.hepl.benbear.iobrep.LoginPacket;
 import be.hepl.benbear.iobrep.LoginResponsePacket;
 import be.hepl.benbear.iobrep.Packet;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
@@ -13,43 +15,62 @@ import java.util.UUID;
 import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.ExecutorService;
 
-public class BoatServer {
+public class BoatServer extends Server<ObjectInputStream, ObjectOutputStream> {
 
-    private final Server server;
-    private final ExecutorService executorService;
     private final Set<UUID> sessions;
-    private volatile boolean closed = false;
 
-    public BoatServer(int port, ExecutorService executorService) {
-        this.server = new Server(port);
-        this.executorService = executorService;
+    public BoatServer(int port, ExecutorService threadPool) {
+        super(
+            port,
+            Thread::new,
+            threadPool,
+            UncheckedLambda.function(ObjectInputStream::new, Throwable::printStackTrace),
+            UncheckedLambda.function(ObjectOutputStream::new, Throwable::printStackTrace)
+        );
         this.sessions = new CopyOnWriteArraySet<>();
     }
 
-    public void start() {
-        while(!closed) {
-            Socket socket = server.accept();
-            executorService.submit(() -> {
-                try(ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-                    ObjectInputStream ois = new ObjectInputStream(socket.getInputStream())) {
-                    while(!Thread.interrupted()) {
-                        Object o = ois.readObject();
-                        if(!(o instanceof Packet)) {
-                            continue;
-                        }
-                        Packet p = (Packet) o;
-                        if(p instanceof AuthenticatedPacket) {
-                            if(!sessions.contains(((AuthenticatedPacket) p).getSession())) {
-                                oos.writeObject(new LoginResponsePacket(null, "Invalid session"));
-                            }
-                        }
-                    }
-                }
-
-                // Infers to Callable and allows to ignore exceptions
-                return null;
-            });
+    @Override
+    protected void read(ObjectInputStream is, ObjectOutputStream os) throws IOException {
+        Object o;
+        try {
+            o = is.readObject();
+        } catch(ClassNotFoundException e) {
+            return;
         }
+
+        switch(((Packet) o).getId()) {
+            case LOGIN:
+                handleLogin((LoginPacket) o, os);
+                break;
+            case GET_CONTAINERS:
+                break;
+            case CONTAINER_OUT:
+                break;
+            case CONTAINER_OUT_END:
+                break;
+            case BOAT_ARRIVED:
+                break;
+            case CONTAINER_IN:
+                break;
+            case CONTAINER_IN_END:
+                break;
+            default:
+                System.err.println("unhandled packet: " + ((Packet) o).getId().name() + " (" + o.getClass().getName() + ")");
+        }
+        os.flush();
+    }
+
+    private void handleLogin(LoginPacket packet, ObjectOutputStream os) throws IOException {
+        System.out.printf("%s:%s%n", packet.getUsername(), packet.getPassword());
+        UUID session = UUID.randomUUID();
+        sessions.add(session);
+        os.writeObject(new LoginResponsePacket(session, null));
+    }
+
+    @Override
+    protected void onClose(Socket socket, Exception e) {
+        e.printStackTrace();
     }
 
 }
