103a4f1f3b14af12da5d0e6241af3ec4ffbd4b54
bendem <online@bendem.be>
2015-10-28 15:20:30 +0100

    Add the container id to ContainerOutResponsePacket

--- iobrep/src/main/java/be/hepl/benbear/iobrep/ContainerOutResponsePacket.java
+++ iobrep/src/main/java/be/hepl/benbear/iobrep/ContainerOutResponsePacket.java
@@ -2,8 +2,15 @@ package be.hepl.benbear.iobrep;
 
 public class ContainerOutResponsePacket extends ResponsePacket {
 
-    public ContainerOutResponsePacket(String reason) {
+    private final String containerId;
+
+    public ContainerOutResponsePacket(String containerId, String reason) {
         super(PacketId.CONTAINER_OUT_RESPONSE, reason);
+        this.containerId = containerId;
+    }
+
+    public String getContainerId() {
+        return containerId;
     }
 
 }
