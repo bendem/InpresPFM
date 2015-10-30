3e6d8859ee8dbce50251938b5ad3abf96ad0cef0
Curlybear <erwanflohic@gmail.com>
2015-10-30 00:49:05 +0000

    Add custom view for the ContainerArrayAdapter

--- BoatApp/app/src/main/java/be/hepl/benbear/boatapp/ContainerArrayAdapter.java
+++ BoatApp/app/src/main/java/be/hepl/benbear/boatapp/ContainerArrayAdapter.java
@@ -1,9 +1,12 @@
 package be.hepl.benbear.boatapp;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
-import java.util.HashMap;
 import java.util.List;
 
 import be.hepl.benbear.iobrep.Container;
@@ -15,4 +18,14 @@ public class ContainerArrayAdapter extends ArrayAdapter<Container> {
         super(context, textViewResourceId, objects);
     }
 
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) getContext()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.container_row_layout, parent, false);
+        ((TextView) rowView.findViewById(R.id.textViewContainerId)).setText(this.getItem(position).getId());
+        ((TextView) rowView.findViewById(R.id.textViewContainerDestination)).setText(this.getItem(position).getDestination());
+        ((TextView) rowView.findViewById(R.id.textViewDateArrival)).setText(this.getItem(position).getArrival().toString());
+        return rowView;
+    }
 }
