139b1fbaafc1fc61dd64c483a75dfaa5ba5517f5
bendem <online@bendem.be>
2015-11-03 18:48:39 +0100

    Add a thing to load db drivers

--- java-commons/src/main/java/be/hepl/benbear/commons/db/Database.java
+++ java-commons/src/main/java/be/hepl/benbear/commons/db/Database.java
@@ -4,6 +4,27 @@ import java.util.Set;
 
 public interface Database extends AutoCloseable {
 
+    enum Driver {
+
+        ORACLE("oracle.jdbc.driver.OracleDriver"),
+        ;
+
+        public final String fqdn;
+
+        Driver(String fqdn) {
+            this.fqdn = fqdn;
+        }
+
+        public void load() {
+            try {
+                Class.forName(fqdn);
+            } catch(ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
     Database connect(String jdbc, String username, String password);
 
     <T> Database registerClass(Class<T> clazz);
