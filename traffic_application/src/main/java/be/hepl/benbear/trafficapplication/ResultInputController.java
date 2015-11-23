ef606adf39b9c3f4d9d94886607b9e590f7546dd
bendem <online@bendem.be>
2015-11-23 02:04:53 +0100

    Fix ResultInput table display

--- traffic_application/src/main/java/be/hepl/benbear/trafficapplication/ResultInputController.java
+++ traffic_application/src/main/java/be/hepl/benbear/trafficapplication/ResultInputController.java
@@ -2,10 +2,13 @@ package be.hepl.benbear.trafficapplication;
 
 import be.hepl.benbear.protocol.tramap.ContainerPosition;
 import be.hepl.benbear.protocol.tramap.InputResultPacket;
+import javafx.beans.property.ReadOnlyIntegerWrapper;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 import java.io.IOException;
@@ -17,8 +20,11 @@ public class ResultInputController implements Initializable {
     private final MainApplication app;
     private MainController mainController;
     private InputResultPacket packet;
-    @FXML private Button okButton;
     @FXML private TableView<ContainerPosition> resultTableView;
+    @FXML private TableColumn<ContainerPosition, String> idColumn;
+    @FXML private TableColumn<ContainerPosition, Number> xColumn;
+    @FXML private TableColumn<ContainerPosition, Number> yColumn;
+    @FXML private Button okButton;
     @FXML private Label statusLabel;
 
     public ResultInputController(MainApplication app) {
@@ -27,6 +33,9 @@ public class ResultInputController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        idColumn.setCellValueFactory(feature -> new ReadOnlyObjectWrapper<>(feature.getValue().getContainerId()));
+        xColumn.setCellValueFactory(feature -> new ReadOnlyIntegerWrapper(feature.getValue().getX()));
+        yColumn.setCellValueFactory(feature -> new ReadOnlyIntegerWrapper(feature.getValue().getY()));
 
         okButton.setOnAction(e -> onOk());
         try {
