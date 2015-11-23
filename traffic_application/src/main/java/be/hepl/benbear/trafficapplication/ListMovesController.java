226448baaa8e5f32a40eb6e36727735d3c042d8c
bendem <online@bendem.be>
2015-11-23 02:12:29 +0100

    Fix ListMoves table display

--- traffic_application/src/main/java/be/hepl/benbear/trafficapplication/ListMovesController.java
+++ traffic_application/src/main/java/be/hepl/benbear/trafficapplication/ListMovesController.java
@@ -2,14 +2,18 @@ package be.hepl.benbear.trafficapplication;
 
 import be.hepl.benbear.protocol.tramap.ListOperationsResponsePacket;
 import be.hepl.benbear.protocol.tramap.Movement;
+import javafx.beans.property.ReadOnlyIntegerWrapper;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.Instant;
 import java.util.ResourceBundle;
 
 public class ListMovesController implements Initializable {
@@ -18,9 +22,15 @@ public class ListMovesController implements Initializable {
     private MainController mainController;
     private ListOperationsResponsePacket packet;
 
-    @FXML private Button okButton;
-    @FXML private TableView<Movement> movementsTableView;
     @FXML private Label descLabel;
+    @FXML private TableView<Movement> movementsTableView;
+    @FXML private TableColumn<Movement, Number> idColumn;
+    @FXML private TableColumn<Movement, String> containerIdColumn;
+    @FXML private TableColumn<Movement, String> destinationColumn;
+    @FXML private TableColumn<Movement, String> companyColumn;
+    @FXML private TableColumn<Movement, Instant> arrivalColumn;
+    @FXML private TableColumn<Movement, Instant> departureColumn;
+    @FXML private Button okButton;
 
     public ListMovesController(MainApplication app) {
         this.app = app;
@@ -28,6 +38,12 @@ public class ListMovesController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        idColumn.setCellValueFactory(f -> new ReadOnlyIntegerWrapper(f.getValue().getMovementId()));
+        containerIdColumn.setCellValueFactory(f -> new ReadOnlyObjectWrapper<>(f.getValue().getContainerId()));
+        destinationColumn.setCellValueFactory(f -> new ReadOnlyObjectWrapper<>(f.getValue().getDestination()));
+        companyColumn.setCellValueFactory(f -> new ReadOnlyObjectWrapper<>(f.getValue().getCompany_name()));
+        arrivalColumn.setCellValueFactory(f -> new ReadOnlyObjectWrapper<>(f.getValue().getDateArrival()));
+        departureColumn.setCellValueFactory(f -> new ReadOnlyObjectWrapper<>(f.getValue().getDateDeparture()));
         okButton.setOnAction(e -> onOk());
 
         try {
